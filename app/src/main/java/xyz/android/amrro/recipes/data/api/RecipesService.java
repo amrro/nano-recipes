1a65ce8e07242cd8dd26e429b181023be0cc795f
Amro Elghobary <amr.elghobary@gmail.com>
2017-08-09 17:19:17 +0200

    add CallAdapter for retrofit

--- app/src/main/java/xyz/android/amrro/recipes/data/api/RecipesService.java
+++ app/src/main/java/xyz/android/amrro/recipes/data/api/RecipesService.java
@@ -1,10 +1,22 @@
 package xyz.android.amrro.recipes.data.api;
 
 
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import xyz.android.amrro.recipes.data.model.Recipe;
+
 /**
  * Created by amrro <amr.elghobary@gmail.com> on 7/22/17.
+ *
+ * Service to make the API requests.
  */
 
 public interface RecipesService {
+
+    @GET("baking.json")
+    public LiveData<ApiResponse<List<Recipe>>> recipes();
 
 }
---
+++ app/src/main/java/xyz/android/amrro/recipes/utils/retrofit/LiveDataCallAdapter.java
@@ -0,0 +1,60 @@
+package xyz.android.amrro.recipes.utils.retrofit;
+
+
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.lang.reflect.Type;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import retrofit2.Call;
+import retrofit2.CallAdapter;
+import retrofit2.Callback;
+import retrofit2.Response;
+import xyz.android.amrro.recipes.data.api.ApiResponse;
+
+/**
+ * A Retrofit adapter that converts the Call into a LiveData of ApiResponse.
+ *
+ * @param <R>
+ */
+public final class LiveDataCallAdapter<R> implements CallAdapter<R, LiveData<ApiResponse<R>>> {
+    private final Type responseType;
+
+    LiveDataCallAdapter(@NonNull final Type responseType) {
+        this.responseType = responseType;
+    }
+
+    @Override
+    public Type responseType() {
+        return responseType;
+    }
+
+    @Override
+    public LiveData<ApiResponse<R>> adapt(@NonNull final Call<R> call) {
+        return new LiveData<ApiResponse<R>>() {
+            AtomicBoolean started = new AtomicBoolean(false);
+
+            @Override
+            protected void onActive() {
+                super.onActive();
+                if (started.compareAndSet(false, true)) {
+                    call.enqueue(new Callback<R>() {
+                        @Override
+                        public void onResponse(@NonNull Call<R> call, @NonNull Response<R> response) {
+                            if (response.isSuccessful() && response.body() != null) {
+                                //noinspection ConstantConditions
+                                postValue(new ApiResponse<R>(response.body()));
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(@NonNull Call<R> call, @NonNull Throwable throwable) {
+                            postValue(new ApiResponse<R>(throwable));
+                        }
+                    });
+                }
+            }
+        };
+    }
+}
