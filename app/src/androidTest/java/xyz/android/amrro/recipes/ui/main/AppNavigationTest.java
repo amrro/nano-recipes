a0bc7618300cc0aa610a22b1d5b4cf530e14bc40
Amro Elghobary <amr.elghobary@gmail.com>
2017-11-01 03:30:34 +0200

    Better tests.

--- app/src/androidTest/java/xyz/android/amrro/recipes/ui/main/AppNavigationTest.java
+++ app/src/androidTest/java/xyz/android/amrro/recipes/ui/main/AppNavigationTest.java
@@ -17,6 +17,7 @@ import com.jakewharton.espresso.OkHttp3IdlingResource;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -33,6 +34,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
@@ -41,6 +43,7 @@ import static org.hamcrest.Matchers.not;
 @RunWith(AndroidJUnit4.class)
 public class AppNavigationTest {
 
+    private static final String RECIPE_INTRODUCTION = "Recipe Introduction";
     @Rule
     public ActivityTestRule<HomeActivity> homeRule = new ActivityTestRule<>(HomeActivity.class);
     private IdlingResource idlingResource;
@@ -76,14 +79,40 @@ public class AppNavigationTest {
     }
 
     @Test
-    public void appNavigationTest() {
-        ViewInteraction recyclerView = onView(
+    public void navigationTest() {
+
+        ViewInteraction recipesList_HomeActivity = onView(
                 allOf(withId(R.id.recycler),
                         childAtPosition(
                                 withClassName(is("android.support.constraint.ConstraintLayout")),
                                 1)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
 
+        recipesList_HomeActivity.check(matches(isDisplayed()));
+
+        // checking the toolbar's title in HomeActivity.
+        onView(allOf(
+                withText("Recipes"),
+                childAtPosition(allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(IsInstanceOf.instanceOf(android.widget.LinearLayout.class), 0)),
+                        0),
+                isDisplayed())
+        ).check(matches(withText("Recipes")));
+
+        recipesList_HomeActivity.perform(actionOnItemAtPosition(0, click()));
+
+        ViewInteraction recipeStepsList = onView(allOf(
+                withId(R.id.recycler_view),
+                isDisplayed(),
+                withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))
+        );
+        recipeStepsList.check(matches(isDisplayed()));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.largeLabel), withText("Ingredients"),
+                        isDisplayed()));
+
+        textView3.check(matches(withText("Ingredients")));
 
         ViewInteraction bottomNavigationItemView = onView(
                 allOf(withId(R.id.nav_steps),
@@ -95,17 +124,55 @@ public class AppNavigationTest {
                         isDisplayed()));
         bottomNavigationItemView.perform(click());
 
-        ViewInteraction recyclerView2 = onView(
-                allOf(withId(R.id.recycler_view),
-                        isDisplayed(),
-                        withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        // step index starts with 0.
+        onView(allOf(
+                withId(R.id.step_number), withText("0"),
+                childAtPosition(
+                        childAtPosition(
+                                IsInstanceOf.instanceOf(android.widget.FrameLayout.class),
+                                0),
+                        0),
+                isDisplayed())
+        ).check(matches(withText("0")));
 
-        recyclerView2.perform(actionOnItemAtPosition(0, click()));
+        // step name displayed.
+        onView(allOf(
+                withId(R.id.step_name),
+                withText(RECIPE_INTRODUCTION),
+                childAtPosition(
+                        childAtPosition(
+                                IsInstanceOf.instanceOf(android.widget.FrameLayout.class),
+                                0),
+                        1),
+                isDisplayed())
+        ).check(matches(isDisplayed()));
 
 
-        // check if the next button is displayed.
-        onView(allOf(withId(R.id.next), isDisplayed())).check(matches(isDisplayed()));
-        // check if the prev button is not displayed cuz it's the first step.
-        onView(withId(R.id.previous)).check(matches(not(isDisplayed())));
+        recipeStepsList.perform(actionOnItemAtPosition(0, click()));
+
+        // check step name
+        onView(allOf(withId(R.id.step_name), isDisplayed()))
+                .check(matches(withText(RECIPE_INTRODUCTION)));
+
+
+        onView(allOf(withId(R.id.previous), not(isDisplayed())))
+                .check(matches(not(isDisplayed())));
+
+        onView(allOf(withId(R.id.next), isDisplayed()))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+
+        onView(allOf(
+                withId(R.id.previous),
+                childAtPosition(
+                        allOf(withId(R.id.linearLayout),
+                                childAtPosition(
+                                        withId(R.id.step_detail),
+                                        1)),
+                        0),
+                isDisplayed())
+        ).check(matches(isDisplayed()));
+
     }
 }
